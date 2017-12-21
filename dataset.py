c27e8149cd140284a772336b7c98376025bab201
duinodu <472365351duino@gmail.com>
2017-12-21 11:28:05 +0800

    update

--- dataset.py
+++ dataset.py
@@ -6,6 +6,20 @@ data_root = os.path.join(os.environ['HOME'], 'data')
 if not os.path.exists(data_root):
     os.makedirs(data_root)
 
+# Resize or Scale depending on pytorch version
+def istorch_0_3():
+    import torch
+    version = torch.__version__[2]
+    if int(version) >= 3:
+        return True
+    else:
+        return False
+    
+if istorch_0_3():
+    resize = resize
+else:
+    resize = transforms.Scale
+
 def create_dataset(dataset_name, root=data_root, image_size=None):
     """Create dataset given dataset name. 
 
@@ -31,21 +45,21 @@ def create_dataset(dataset_name, root=data_root, image_size=None):
     elif dataset_name == 'cifar10':
         dataset = dset.CIFAR10(root=path, download=True,
                                transform=transforms.Compose([
-                                   transforms.Resize(image_size),
+                                   resize(image_size),
                                    transforms.ToTensor(),
                                    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
                                ]))
     elif dataset_name == 'cifar100':
         dataset = dset.CIFAR10(root=path, download=True,
                                transform=transforms.Compose([
-                                   transforms.Resize(image_size),
+                                   resize(image_size),
                                    transforms.ToTensor(),
                                    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
                                ]))
     elif dataset_name == 'lsun':
         dataset = dset.LSUN(db_path=path, classes=['bedroom_train'],
                             transform=transforms.Compose([
-                                transforms.Resize(image_size),
+                                resize(image_size),
                                 transforms.CenterCrop(image_size),
                                 transforms.ToTensor(),
                                 transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
